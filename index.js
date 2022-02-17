03e46ab9db0547585fa9d760adc7ace0d58b26d1
Andrew Bradt <andrewjbradt@gmail.com>
2022-02-17 14:57:48 -0500

    Implemented fetchMyIp

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ fetchMyIP((error, ip)=>{
   if (error) {
     return console.log("It didn't work!", error);
   }
-
   console.log('It worked! Returned IP: ', ip);
 });
--- iss.js
+++ iss.js
@@ -1,5 +1,15 @@
-const fetchMyIP = (callback) => {
+const request = require('request');
 
+const fetchMyIP = (callback) => {
+  const url = 'https://api.ipify.org/?format=json';
+  request(url, (e, res, body) => {
+    if (e) {
+      return callback(e, null);
+    } else {
+      const {ip} = JSON.parse(body);
+      return callback(null, ip);
+    }
+  });
 };
 
 module.exports = {
