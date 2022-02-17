a5a5e1bbcb06acf3edd189f62979a5759c6d380d
Andrew Bradt <andrewjbradt@gmail.com>
2022-02-17 14:52:06 -0500

    Added fetchMyIP declarationa nd export in iss.js, imported and invoked in index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const { fetchMyIP } = require('./iss');
+
+fetchMyIP((error, ip)=>{
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+
+  console.log('It worked! Returned IP: ', ip);
+});
---
+++ iss.js
@@ -0,0 +1,7 @@
+const fetchMyIP = (callback) => {
+
+};
+
+module.exports = {
+  fetchMyIP
+};
